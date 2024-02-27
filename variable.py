5a5cefec23002d0c3864cca237edbad28f154a85
Nikhil Arya <58100439+nikhil123arya@users.noreply.github.com>
2024-02-27 11:52:41 +0530

    Variables in python

---
+++ variable.py
@@ -0,0 +1,9 @@
+# variables in python
+
+cal_to_seconds = 24 * 60 * 60
+cal_to_minutes = 24 * 60
+cal_to_hours = 24
+
+print(f"20 days are {20 * cal_to_seconds} seconds")
+print(f"35 days are {35 * cal_to_hours} hours")
+print(f"40 days are {40 * cal_to_minutes} minutes")

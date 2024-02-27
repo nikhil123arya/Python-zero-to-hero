39a92b02f67325d4e62cb170e085ceeb42d5e75e
Nikhil Arya <58100439+nikhil123arya@users.noreply.github.com>
2024-02-27 11:06:06 +0530

    First Program

---
+++ hello.py
@@ -0,0 +1,10 @@
+# print('hello world')
+# print('200 is a great number')
+# print(20)
+# print(20.13)
+
+# string concatination
+print("20 days are " + str(50) +" minutes")
+
+print(f"20 days are {20 * 24 * 60} minutes")
+print(f"35 days are {35 * 24 * 60} minutes")
